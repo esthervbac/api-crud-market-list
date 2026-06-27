0dbca0d41b9593da6b42d9dbbee0191ff92133fe
Esther V Bacelar <esther.v.bacelar@gmail.com>
2026-06-27 00:50:33 -0300

    feat(): Removed console.log

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -5,8 +5,6 @@ dotenv.config();
 
 export async function connectDatabase() {
   try {
-    console.log("URI:", JSON.stringify(process.env.MONGO_URI));
-
     await mongoose.connect(process.env.MONGO_URI!);
 
     console.log("MongoDB conectado com sucesso!");
